dd8576f054608f1d732aac9cd1a6be455488a0bd
Maximilian Mauroner <github@mauroner.eu>
2023-01-01 12:25:40 +0100

    changed switch color
    Took 1 minute

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -131,13 +131,13 @@ const ManageFriends = ({ friend, mumtatePermissions }: {
         </label>
       </div>
       <div className={"flex gap-2.5 justify-center py-2"}>
-        <label className="inline-flex  items-center  cursor-pointer">
+        <label className="inline-flex items-center cursor-pointer">
           <span className="mr-3 text-sm font-medium text-gray-900 dark:text-gray-300">Emergency Contact</span>
           <div className={"relative inline-flex items-center cursor-pointer"}>
             <input type="checkbox" value="" className="sr-only peer" checked={emergencyContact}
                    onChange={() => updatePermissions(allowLocationSharing, !emergencyContact)} />
             <div
-              className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-0  dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"></div>
+              className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-0 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
           </div>
         </label>
       </div>
